f0e4f2b2b3a5d794df7e3e0ff8f902c3025316a7
yqian44 <yqian44@gatech.edu>
2015-10-18 00:23:07 -0400

    Oct 18 from surface

--- temp.rb
+++ temp.rb
@@ -1,28 +1,36 @@
-# @param {String} input
-# @return {Integer[]}
-def diff_ways_to_compute(input)
-    ret = []
-    (0..input.length-1).each do |i|
-        if "+-*".include? input[i]
-            part1, part2 = input[0..i-1], input[i+1..-1]
-            part1_ret = diff_ways_to_compute(part1)
-            part2_ret = diff_ways_to_compute(part2)
-            part1_ret.each do |x|
-                part2_ret.each do |y|
-                    c = 0
-                    case input[i]
-                    when "+"
-                        c = x+y
-                    when "-"
-                        c = x-y
-                    when "*"
-                        c = x*y
-                    end
-                    ret << c    
-                end
-            end
+def plausible(str)
+    dict = []
+    return dict[str]
+end
+
+def p_recur(str, arr = nil, prev_i = 1)
+    arr = Array.new(str.length, nil) if arr.nil?
+    return 0 if str.empty?
+    max_p, max_i = 0-2**31, nil
+    (str.length-prev_i).downto(0) do |i|
+        if arr[i].nil?
+            p_recur(str, arr, i)
+        end
+        if max_p < arr[i]+plausible(str[i+1..-1])
+            max_p, max_i = arr[i]+plausible(str[i+1..-1]), i
         end
     end
-    ret << input.to_i if ret.size == 0
-    ret
+    return max_p
 end
+
+def p_iter(str)
+    arr = Array.new(str.length, nil)
+    max_p = 0-2**31
+    (0..str.length-1).each do |i|
+        if i == 0
+            arr[i] = plausible(str[i])
+        else
+            (0..i-1).each do |j|
+                max_p = [max_p, arr[j]].max
+            end
+            arr[i] = max_p + plausible(str[j+1..i])
+        end
+    end
+    return arr[-1]
+end
+
